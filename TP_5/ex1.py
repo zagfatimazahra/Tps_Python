e1bb1626d7ed571a2580b039a049993afb3fae70
zagfatimazahra <fatimazahra.zagrane@gmail.com>
2025-01-09 14:07:18 +0100

    Update ex1.py

--- TP_5/ex1.py
+++ TP_5/ex1.py
@@ -1,1 +1,7 @@
-
+with open("C:\\Users\\Administrator\\Desktop\\Study\\CODES\\TPs_Python\\TP 5\\exemple.txt","r") as fichier:
+    ligne=fichier.readline()
+    i=1;
+    while ligne:
+        print(i,"-",ligne.strip())
+        i+=1;
+        ligne=fichier.readline()
