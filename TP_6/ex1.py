9410f9fd128ac58de455abd028ff3fceb8eadbe2
zagfatimazahra <fatimazahra.zagrane@gmail.com>
2025-01-22 01:40:03 +0100

    Update ex1.py

--- TP_6/ex1.py
+++ TP_6/ex1.py
@@ -1,1 +1,9 @@
+def safe_division(a, b):
+    try:
+        result = a / b
+        print("Résultat:", result)
+    except ZeroDivisionError:
+        print("Erreur : Division par zéro.")
 
+safe_division(8,2)
+safe_division(8,0)
