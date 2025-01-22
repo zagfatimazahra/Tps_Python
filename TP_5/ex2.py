86a176fb4e874250d644a8f83233b55e29211a98
zagfatimazahra <fatimazahra.zagrane@gmail.com>
2025-01-22 01:38:17 +0100

    Update ex2.py

--- TP_5/ex2.py
+++ TP_5/ex2.py
@@ -1,4 +1,4 @@
-with open("C:\\Users\\Administrator\\Desktop\\Study\\CODES\\TPs_Python\\TP 5\\phrases.txt", "w") as file:
+with open("phrases.txt", "w") as file:
         for i in range(3):
             phrase = input(f"Entrez la phrase {i + 1} : ")
             file.write(phrase + "\n")
